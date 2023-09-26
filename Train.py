e1656ef605fb176deb08593f34706125a7075c1f
pinu <pinudash104@gmail.com>
2023-09-27 01:36:37 +0530

    Part 5

--- Train.py
+++ Train.py
@@ -2,6 +2,8 @@ import json
 import tensorflow as tf
 import numpy as np
 import Text_processing as T_process
+from tensorflow import keras
+import pickle
 
 Data=json.load(open("./Data.json"))
 Tags=[]
@@ -10,6 +12,7 @@ for i in Data["intents"]:
 
 # print(len(Tags))
 
+Tags=sorted(set(Tags))
 
 X,Y=[],[]
 Vocab=[]
@@ -35,7 +38,6 @@ ignore_symbole=[",","?","/",".","!","|"]
 Vocab=[T_process.Stem(w) for w in Vocab if w not in ignore_symbole]
 
 Vocab=sorted(set(Vocab))
-Tags=sorted(set(Tags))
 
 
 for i,x in enumerate(X):
@@ -44,6 +46,34 @@ for i,x in enumerate(X):
 X=np.array(X)
 Y=np.array(Y)
 
-print(X.shape,Y.shape)
-print(X[0])
 
+def Neural_Network(input_shape,output_shape):
+    model=keras.Sequential([
+        keras.layers.Dense(256,activation="relu",input_shape=input_shape), #Input Layer
+        #Hidden layer
+        keras.layers.Dense(128,activation="relu"),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(64,activation="relu"),
+        keras.layers.Dense(16,activation="relu"),
+        keras.layers.Dropout(0.2),
+        #outpu layer
+        keras.layers.Dense(output_shape,activation="softmax")
+    ])
+    model.compile(optimizer="adam",loss=keras.losses.SparseCategoricalCrossentropy(),metrics=["accuracy"])
+    return model
+
+MINI=Neural_Network(input_shape=(len(X[0]),) , output_shape=len(Tags))
+
+
+MINI.fit(X,Y,epochs=150)
+
+
+
+MINI_data={
+    "Architecture":MINI.to_json(),
+    "Weight":MINI.get_weights(),
+    "Vocab":Vocab,
+    "Tags":Tags
+}
+
+pickle.dump(MINI_data,open("./MINI.pkl","wb"))
