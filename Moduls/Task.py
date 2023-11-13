25f71fc6313834d741479a0a9b81e318921b6a20
pinu <pinudah104@gmail.com>
2023-11-13 19:53:56 +0530

    Part 11.1

--- Moduls/Task.py
+++ Moduls/Task.py
@@ -1,5 +1,11 @@
 from datetime import datetime,date,timedelta
 import pywhatkit
+import wolframalpha # pip install wolframalpha
+import wikipedia
+from Moduls import Personal
+
+Wolfarm_app=wolframalpha.Client(Personal.WolfarnID)
+
 
 def Task(response_given,UserInput_str):
     response=str(response_given).lower()
@@ -20,5 +26,46 @@ def Task(response_given,UserInput_str):
     elif "music" in response:
         pywhatkit.playonyt(UserInput_str)
         return "Playing your song"
+    
+    elif "math" in response or "squestions" in response:
+        try:
+            return Wolfarm_Alpha(UserInput_str)
+        except:
+            ignore_word=["wiki","wikipedia","search","tell","about","me","something"]
+            for i in ignore_word:
+                UserInput_str=UserInput_str.replace(i,"")
+            return Wiki(UserInput_str)
 
     return response_given
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def Wolfarm_Alpha(quary):
+    response=Wolfarm_app.query(quary)
+    response=(next(response.results)).text
+    return response
+
+def Wiki(quary):
+    try:
+        return wikipedia.summary(quary,2)
+    except:
+        return "Having some problem while fetching data."
+
+
+
+
+
